19b0db9ec13368c2ef27a23e5328ebc5f341d1cd
Dan Pickett <dan.pickett@launchacademy.com>
2016-03-03 17:37:14 -0500

    initial commit

---
+++ lib/adjutant/ruby.rb
@@ -0,0 +1,7 @@
+require "adjutant/ruby/version"
+
+module Adjutant
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/adjutant/ruby/version.rb
@@ -0,0 +1,5 @@
+module Adjutant
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
